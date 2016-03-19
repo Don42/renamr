1c183afe6b5616eac314bb4ef2c73bb843f5e92c
Marco 'don' Kaulea <DonMarco42@gmail.com>
2016-03-19 19:42:02 +0100

    Switch docstring format

--- renamr.py
+++ renamr.py
@@ -63,29 +63,25 @@ class NoRegexMatchException(ValueError):
     pass
 
 
-def get_series_name(file_path):
+def get_series_name(file_path: pl.Path) -> str:
     """Gets the seriesname from the directory structure
 
-    Args:
-        file_path (pathlib.Path): Path to the file
-
-    Returns:
-        (string): Name of the series
-
+    :param file_path: Path to the file
+    :type file_path: (pathlib.Path)
+    :return: Name of the series
+    :rtype: str
     """
     series_name = file_path.parts[-3]
     return series_name
 
 
-def get_identifier(file_path):
+def get_identifier(file_path: pl.Path) -> EpisodeIdent:
     """Tries multiple regexes to get season and episode number
 
-    Args:
-        file_path (pathlib.Path): Absolute path to file
-
-    Returns:
-        (EpisodeIdent): Identifing the episode
-
+    :param file_path: Absolute path to the file
+    :type file_path: pathlib.Path
+    :returns: Identifing the episode
+    :rtype: EpisodeIdent
     """
     for regex in regexes:
         match = re.search(regex, file_path.name)
@@ -96,15 +92,13 @@ def get_identifier(file_path):
     raise NoRegexMatchException()
 
 
-def build_identifier(identifier):
-    """Builds the episode identifier, consisting of season and episodenumber
+def build_identifier(identifier: EpisodeIdent) -> str:
+    """Builds the episode identifier, consisting of season and episode number
 
-    Args:
-        identifier (EpisodeIdent): Identifier of a episode
-
-    Returns:
-        (string): Identifing the episode
-
+    :param identifier: Identifier of a episode
+    :type identifier: EpisodeIdent
+    :returns: String identifier for the episode
+    :rtype: str
     """
     if identifier.season < 0 or identifier.episode < 0:
         raise ValueError("Season and Episode can't be negative")
@@ -112,7 +106,12 @@ def build_identifier(identifier):
     return ret
 
 
-def download_series_page(short_name):
+def download_series_page(short_name: str) -> str:
+    """
+
+    :param short_name: Short name of a series
+    :return: Downloaded page
+    """
     session = requests.Session()
     payload = {'q': short_name, 'embed': 'episodes'}
     response = session.get(SINGLESEARCH_SHOWS_URL, params=payload)
@@ -127,6 +126,11 @@ def download_series_page(short_name):
 
 
 def extract_episode_name_mapping(series_page):
+    """
+
+    :param series_page: Page to parse
+    :return: mapping from season/episode to name
+    """
     content = json.loads(series_page)
     episodes = content['_embedded']['episodes']
     ident_name_mapping = dict()
@@ -136,6 +140,11 @@ def extract_episode_name_mapping(series_page):
 
 
 def get_series_data(series_name):
+    """
+
+    :param series_name: Name of the series
+    :return: mapping for the series
+    """
     short_name = series_name.replace(' ', '-')
     if short_name not in cache:
         page = download_series_page(short_name)
@@ -144,40 +153,47 @@ def get_series_data(series_name):
 
 
 def get_episode_name(ident, series_data):
+    """
+
+    :param ident: Identifier for the episode
+    :param series_data: mapping for the series
+    :return: episode name
+    """
     try:
         return series_data[ident]
     except IndexError:
-        pass
-    return ""
+        return ""
 
 
-def get_partial_path(path):
+def get_partial_path(path: pl.Path) -> list:
     """Shortens the path for output.
 
     Returns only last two folderlevels
     and the filename
 
-    Args:
-        path (pathlib.Path): Path to a file
-
-    Returns:
-        (list): containing the last three path elements
+    :param path: Path to a file
+    :type path: pathlib.Path
+    :return: last three path elements
+    :rtype: list
 
     """
     return path.parts[-3:]
 
 
-def make_new_path(series_name, ident, ep_name, old_path):
+def make_new_path(series_name: str, ident: EpisodeIdent, ep_name: str, old_path: pl.Path):
     """Generate new path for file
 
-    Args:
-        series_name (string): Name of the series
-        ident (EpisodeIdent): Identifier of the episode
-        ep_name (string): Name of the episode
-        old_path (pathlib.Path): Path to the file
+    :param series_name: Name of the series
+    :type series_name: str
+    :param ident: Identifier of the episode
+    :type ident: EpisodeIdent
+    :param ep_name: Name of the episode
+    :type ep_name: str
+    :param old_path: Path to the file
+    :type old_path: pathlib.Path
 
-    Returns:
-        (pathlib.Path): new file path
+    :return: new file path
+    :rtype: pathlib.Path
 
     """
     if None in (series_name, ident, old_path):
@@ -190,12 +206,13 @@ def make_new_path(series_name, ident, ep_name, old_path):
     return old_path.with_name(new_name)
 
 
-def rename_file(old_path, new_path):
+def rename_file(old_path: pl.Path, new_path: pl.Path):
     """Rename file if it does not already exist
 
-    Args:
-        old_path (pathlib.Path): Path to the file to be renamed
-        new_path (pathlib.Path): Path the file should be renamed to
+    :param old_path: Path to the file to be renamed
+    :type old_path: pathlib.Path
+    :param new_path: Path the file should be renamed to
+    :type new_path: pathlib.Path
 
     """
     if new_path.exists():
@@ -208,14 +225,14 @@ def rename_file(old_path, new_path):
         new=get_partial_path(new_path)))
 
 
-def create_file_list(source):
+def create_file_list(source: list) -> list:
     """Read files from list and check existence
 
-    Args:
-        source (list): List of files to check and make absolute
+    :param source: List of files to check and make absolute
+    :type source: list
 
-    Returns:
-        list: of existing, absolute paths
+    :return: existing, absolute paths in a list
+    :rtype: list
     """
     path_list = [pl.Path(x.replace('\n', '')) for x in source]
     files = filter(pl.Path.exists, path_list)

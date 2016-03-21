7396b2de6292f1ffa44c1115c8fbe4a874e0ca95
Marco 'don' Kaulea <DonMarco42@gmail.com>
2016-03-21 17:29:09 +0100

    Move parser tests to folder

---
+++ tests/test_series_database.py
@@ -0,0 +1,22 @@
+import pytest
+
+import renamr.series_database
+
+
+def test_build_identifier():
+    assert "S01E01" == renamr.series_database.EpisodeIdentifier(1, 1).__str__()
+    assert "S10E20" == renamr.series_database.EpisodeIdentifier(10, 20).__str__()
+    assert "S11E999" == renamr.series_database.EpisodeIdentifier(11, 999).__str__()
+    assert "S00E00" == renamr.series_database.EpisodeIdentifier(0, 0).__str__()
+
+
+def test_build_identifier_errors():
+    with pytest.raises(TypeError):
+        renamr.series_database.EpisodeIdentifier(None).__str__()
+    with pytest.raises(Exception):
+        renamr.series_database.EpisodeIdentifier(15).__str__()
+    with pytest.raises(ValueError):
+        renamr.series_database.EpisodeIdentifier(-1, 5).__str__()
+    with pytest.raises(ValueError):
+        renamr.series_database.EpisodeIdentifier(1, -5).__str__()
+
